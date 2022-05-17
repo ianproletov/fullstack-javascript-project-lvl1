cc4382680e33a14647568c24fc6ff7f100d142c4
ian proletov <yan.proletov@gmail.com>
2022-05-17 23:12:36 +0300

    add brain calc

---
+++ src/games/calc.js
@@ -0,0 +1,23 @@
+import game from '../index.js';
+
+const description = 'What is the result of the expression?';
+
+const operationMap = {
+  0: ['+', (a, b) => a + b],
+  1: ['-', (a, b) => a - b],
+  2: ['*', (a, b) => a * b],
+};
+const operationsNumber = 3;
+const maxValue = 100;
+
+const generateStep = () => {
+  const operationNumber = Math.floor(Math.random() * operationsNumber);
+  const [operationSymbol, operation] = operationMap[operationNumber];
+  const operator1 = Math.floor(Math.random() * maxValue);
+  const operator2 = Math.floor(Math.random() * maxValue);
+  const correctAnswer = operation(operator1, operator2);
+  const question = `${operator1} ${operationSymbol} ${operator2}`;
+  return [question, String(correctAnswer)];
+};
+
+export default () => game(description, generateStep);
